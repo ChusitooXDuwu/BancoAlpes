7b6082ec7ca047d06b029cadc944a93624851cae
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-05-09 11:59:29 -0500

    updated clientesss endpoint

--- cliente/urls.py
+++ cliente/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.cliente_view, name='cliente_view'),
+    path('', views.clientes_view, name='clientes_view'),
     path('<int:pk>', views.cliente_view, name='cliente_view'),
 ]
