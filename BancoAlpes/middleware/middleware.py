f749c2e82547ce5c8269b091117a2abb0f34976b
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-05-10 16:32:39 -0500

    middleware.py

--- BancoAlpes/middleware/middleware.py
+++ BancoAlpes/middleware/middleware.py
@@ -3,17 +3,14 @@
 from django.conf import settings
 
 # middlewares.py
-class CompleteURLMiddleware:
+# middleware.py
+# middleware.py
+class XForwardedPortMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Establecer el esquema de la solicitud basado en 'X-Forwarded-Proto'
-        forwarded_proto = request.META.get('HTTP_X_FORWARDED_PROTO')
-        if forwarded_proto:
-            request.scheme = forwarded_proto
-        
-        # Establecer el host y el puerto basado en 'X-Forwarded-Host' y 'X-Forwarded-Port'
+        # Ajustar el host y el puerto basado en 'X-Forwarded-Host' y 'X-Forwarded-Port'
         forwarded_host = request.META.get('HTTP_X_FORWARDED_HOST')
         forwarded_port = request.META.get('HTTP_X_FORWARDED_PORT')
         if forwarded_host:
@@ -21,5 +18,6 @@ class CompleteURLMiddleware:
                 request.META['HTTP_HOST'] = f'{forwarded_host}:{forwarded_port}'
             else:
                 request.META['HTTP_HOST'] = forwarded_host
-
         return self.get_response(request)
+
+
