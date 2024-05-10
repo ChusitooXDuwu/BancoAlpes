b8b07035e50a4f5aa0d0d2c977149e7f2f19e6ba
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-05-10 16:08:43 -0500

    debo aprender a escribir

--- BancoAlpes/settings.py
+++ BancoAlpes/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'BacoAlpes.middleware.middleware.UseForwardedPortMiddleware',
+    'BancoAlpes.middleware.middleware.UseForwardedPortMiddleware',
 
 ]
 
