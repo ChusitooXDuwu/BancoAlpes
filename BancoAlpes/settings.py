f94b26c1c86adaf5ba5752abb6cf6bcc0ed820e4
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-05-10 16:07:06 -0500

    middleware

--- BancoAlpes/settings.py
+++ BancoAlpes/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'BancoAlpes.middleware.UseForwardedPortMiddleware',
+    'bancoalpes.middleware.middleware.UseForwardedPortMiddleware',
 
 ]
 
