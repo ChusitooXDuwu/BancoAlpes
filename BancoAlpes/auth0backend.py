b215dff01808fe6ae0edefe9950df24cc9fa22b9
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-05-10 10:45:10 -0500

    http modif legal?

--- BancoAlpes/auth0backend.py
+++ BancoAlpes/auth0backend.py
@@ -14,18 +14,18 @@ class Auth0(BaseOAuth2):
 
     def authorization_url(self):
         """Return the authorization endpoint."""
-        return "https://" + self.setting('DOMAIN') + "/authorize"
+        return "http://" + self.setting('DOMAIN') + "/authorize"
 
     def access_token_url(self):
         """Return the token endpoint."""
-        return "https://" + self.setting('DOMAIN') + "/oauth/token"
+        return "http://" + self.setting('DOMAIN') + "/oauth/token"
 
     def get_user_id(self, details, response):
         """Return current user id."""
         return details['user_id']
 
     def get_user_details(self, response):
-        url = 'https://' + self.setting('DOMAIN') + '/userinfo'
+        url = 'http://' + self.setting('DOMAIN') + '/userinfo'
         headers = {'authorization': 'Bearer ' + response['access_token']}
         resp = requests.get(url, headers=headers)
         userinfo = resp.json()
