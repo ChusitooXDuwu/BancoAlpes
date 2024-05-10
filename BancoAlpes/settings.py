2d8f4ed10305f2fe9d010f7923c3d540ad64823b
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-05-10 16:31:26 -0500

    middle whiiuth scheme

--- BancoAlpes/settings.py
+++ BancoAlpes/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'BancoAlpes.middleware.middleware.CompleteURLMiddleware',
+    'BancoAlpes.middleware.middleware.XForwardedPortMiddleware',
 
 ]
 
