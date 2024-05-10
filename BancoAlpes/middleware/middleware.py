f2c240efd56f138b0965e878f2570e6d1ecaa5c7
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-05-10 16:27:34 -0500

    fuul uel middleaa

--- BancoAlpes/middleware/middleware.py
+++ BancoAlpes/middleware/middleware.py
@@ -3,19 +3,23 @@
 from django.conf import settings
 
 # middlewares.py
-class XForwardedPortMiddleware:
+class CompleteURLMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Modificar el protocolo a 'http' usando la cabecera 'X-Forwarded-Proto'
-        request.META['HTTP_X_FORWARDED_PROTO'] = 'http'
+        # Establecer el esquema de la solicitud basado en 'X-Forwarded-Proto'
+        forwarded_proto = request.META.get('HTTP_X_FORWARDED_PROTO')
+        if forwarded_proto:
+            request.scheme = forwarded_proto
         
-        # Ajustar el puerto si viene en la cabecera 'X-Forwarded-Port'
+        # Establecer el host y el puerto basado en 'X-Forwarded-Host' y 'X-Forwarded-Port'
+        forwarded_host = request.META.get('HTTP_X_FORWARDED_HOST')
         forwarded_port = request.META.get('HTTP_X_FORWARDED_PORT')
-        if forwarded_port:
-            host = request.get_host().split(':')[0]
-            request.META['HTTP_HOST'] = f'{host}:{forwarded_port}'
-        response = self.get_response(request)
-        return response
+        if forwarded_host:
+            if forwarded_port:
+                request.META['HTTP_HOST'] = f'{forwarded_host}:{forwarded_port}'
+            else:
+                request.META['HTTP_HOST'] = forwarded_host
 
+        return self.get_response(request)
