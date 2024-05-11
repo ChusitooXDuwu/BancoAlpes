64a9d111d9f6849713752a0d5cdf8ea21a049269
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-05-10 20:01:31 -0500

    middleware try two

--- BancoAlpes/middleware/COOP.py
+++ BancoAlpes/middleware/COOP.py
@@ -3,5 +3,7 @@ from django.utils.deprecation import MiddlewareMixin
 
 class RemoveCOOPHeaderMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
+        print("Modifying response headers to remove COOP")  # Agrega esto para depuración
         response.headers.pop('Cross-Origin-Opener-Policy', None)
         return response
+
