fa9befae2907073188d9bfe07801528795d0af99
chusitooxduwu <w.chu@uniandes.edu.co>
2024-05-30 02:26:12 -0500

    nice

--- BancoAlpes/settings.py
+++ BancoAlpes/settings.py
@@ -16,8 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DOC_ROOT = os.path.join(BASE_DIR, 'docsClientes')
-DOC_URL = '/docsClientes/'
+# DOC_ROOT = os.path.join(BASE_DIR, 'docsClientes')
+# DOC_URL = '/docsClientes/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
