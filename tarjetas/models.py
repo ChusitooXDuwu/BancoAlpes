fbc4cf04cae320577c9911f49675b9276f7caee9
chusitooxduwu <w.chu@uniandes.edu.co>
2024-05-30 01:29:41 -0500

    orale con S mayuscula

--- tarjetas/models.py
+++ tarjetas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class tarjeta(models.Model):
+class Tarjeta(models.Model):
     id = models.AutoField(primary_key=True)
     cliente = models.CharField(max_length=100)
     tipo = models.CharField(max_length=100) #si es cedula, recibo, pasaporte, etc
